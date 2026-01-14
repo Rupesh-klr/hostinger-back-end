17d0756a633c02064cbb3f8dbff733a5055d9cf9
rupeshklr1 <rupeshklr1@gmail.com>
2026-01-14 10:18:16 +0530

    1.02 adding logs folder

---
+++ env_constant.js
@@ -0,0 +1,6 @@
+// env_constant.js
+module.exports = {
+    GOOGLE_CLIENT_ID: '775388863546-huooskea92qrm42qokb2tama5nqe0521.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: '*****',
+    API_COOKIES: 'helloWorld'
+};
