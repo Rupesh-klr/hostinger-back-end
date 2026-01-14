667bdd405947d446f1f9465d54fb56855d380242
rupeshklr1 <rupeshklr1@gmail.com>
2026-01-14 11:42:17 +0530

    1.02 adding logs folder

--- env_constant.js
+++ env_constant.js
@@ -1,6 +1,6 @@
 // env_constant.js
 module.exports = {
-    krishnaleena222_project1_gokgoal_GOOGLE_CLIENT_ID: '775388863546-huooskea92qrm42qokb2tama5nqe0521.apps.googleusercontent.com',
-    krishnaleena222_project1_gokgoal_GOOGLE_CLIENT_SECRET: '***',
+    KRISHNALEENATWOTWOTWO_PROJECTONE_GOKGOAL_GOOGLE_CLIENT_ID: '775388863546-huooskea92qrm42qokb2tama5nqe0521.apps.googleusercontent.com',
+    KRISHNALEENATWOTWOTWO_PROJECTONE_GOOGLE_CLIENT_SECRET: '***',
     API_COOKIES: 'helloWorld'
 };
